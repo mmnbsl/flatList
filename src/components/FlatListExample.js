71309b6e34b342187d9fd2975d0c5b421abd8860
mmnbsl <muminbasol@gmail.com>
2021-01-09 13:56:06 +0300

    ActivityIndicator Added
    while data is fetching, showing indicator.

--- src/components/FlatListExample.js
+++ src/components/FlatListExample.js
@@ -1,76 +1,98 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView, TextInput , View, FlatList, Image, Text, StyleSheet, StatusBar, Platform} from 'react-native';
+import React, { Component } from 'react';
+import { SafeAreaView, TextInput , View, FlatList, Image, Text, StyleSheet, StatusBar, Platform, ActivityIndicator} from 'react-native';
+import axios from 'axios';
 
-const FlatListExample = () =>{
-    const [jsonData, setJsonData] = useState()
-    const [searchText, setSearchText] = useState(null);
+export default class FlatListExample extends Component {
 
-    const mainContainer = Platform.select({
-        ios: style.mainContainerIos,
-        android : style.mainContainerAndroid
-    })
+    constructor(){
+        super()
+        this.state = {
+            jsonData : [], 
+            searchText : '', 
+        }
+    }
 
-    useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/photos')
-        .then(response => response.json())
-        .then(data => setJsonData(data))
-    },[])
+    componentDidMount(){
+        this.getData();
+    }
+    
+    getData = async () => {
+        const { data: { results } } = await axios('https://randomuser.me/api/?results=50')
+        this.setState({
+            jsonData : results
+        })
+    }    
+    render(){
+        const mainContainer = Platform.select({ 
+            ios: style.mainContainerIos,
+            android: style.mainContainerAndroid
+        })
 
-    const _renderItem =( {item} ) => {
-        return(
-            <View style={[style.viewContainer, {backgroundColor : item.id % 2 === 1 ? '#f9f9f9' : ''}]}>   
-                <View style = {style.itemContainer}>
-                    <Image
-                        style={style.avatar}
-                        source={{ uri: item.thumbnailUrl }}
-                    />
-                    <Text>{item.title}</Text> 
+        const _renderItem = ({ item }) => {
+            return (
+                <View style={[style.viewContainer, { backgroundColor: item.id % 2 === 1 ? '#f9f9f9' : '' }]}>
+                    <View style={style.itemContainer}>
+                        <Image
+                            style={style.avatar}
+                            source={{ uri: item.picture.thumbnail }}
+                        />
+                        <Text>{item.name.first}</Text>
+                    </View>
                 </View>
-            </View>
-        )
-    }
+            )
+        }
 
-    const _filter = (text) =>{
-        const newData = jsonData.filter((item)=>{
-            const listItem = `${item.title.toLowerCase()}`; 
-            return listItem.indexOf(text.toLowerCase()) > -1;
-        }) 
-        setJsonData(newData)
-        setSearchText(text)
-    }
+        const _filter = (text) => {
+            const newData = this.state.jsonData.filter((item) => {
+                const listItem = `${item.title.toLowerCase()}`;
+                return listItem.indexOf(text.toLowerCase()) > -1;
+            })
+            this.setState({jsonData : newData})
+            this.setState({searchText : text})
+        }
 
-    const _headerComponent = () =>{
-        return (
-            <View style = {style.inputContainer}>
-                <TextInput
-                    style = {style.input}
-                    placeholder = 'Search..'
-                    value = {searchText}
-                    onChangeText = {(text) => {
-                        //setSearchText(text)
-                        _filter(text)
-                   
-                    }}
-                    keyboardType= 'default' 
+        const _headerComponent = () => {
+            return (
+                <View style={style.inputContainer}>
+                    <TextInput
+                        style={style.input}
+                        placeholder='Search..'
+                        value={this.state.searchText}
+                        onChangeText={(item) => {
+                            //setSearchText(text)
+                            _filter(item)
+
+                        }}
+                        keyboardType='default'
+                    />
+                </View>
+            )
+        }
+        const _footerComponent = () => {
+            return(
+                <View style = {style.indicatorContainer}> 
+                    <ActivityIndicator />
+                </View>
+            )
+        }
+        return ( 
+            <SafeAreaView style={mainContainer}>
+                <FlatList
+                    data={this.state.jsonData}
+                    renderItem={_renderItem}
+                    keyExtractor={(item) => item.login.uuid}
+                    ListHeaderComponent={_headerComponent}
+                    stickyHeaderIndices={[0]} //Header'ı sabitledi
+                    ListFooterComponent = {_footerComponent}
                 />
-            </View>
+            </SafeAreaView>
         )
     }
-    return(
-        <SafeAreaView style = {mainContainer}>
-            <FlatList
-                data = {jsonData}
-                renderItem = {_renderItem}
-                keyExtractor = {(item) => item.id.toString()}
-                ListHeaderComponent = {_headerComponent}
-                stickyHeaderIndices = {[0]} //Header'ı sabitledi
-            />
-        </SafeAreaView>
-    )
 }
 const style = StyleSheet.create({
     mainContainerIos : {
         flex : 1,
+        width : '100%',
         paddingTop : StatusBar.currentHeight
     },
     mainContainerAndroid: {
@@ -104,7 +126,8 @@ const style = StyleSheet.create({
     input : {
         height : 50,
         marginHorizontal :10,
+    },
+    indicatorContainer : {
+        paddingVertical : 10
     }
 })
-
-export default FlatListExample
